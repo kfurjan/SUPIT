147eac17e45676df72ca8f7698c877a604471f27
Kevin Furjan <kevin.furjan@reversinglabs.com>
2019-12-17 10:00:30 +0100

    Testing some stuff

---
+++ assets/js/js_scripts.js
@@ -0,0 +1,5 @@
+import {
+    typedFunction
+} from '../js/modules'
+
+document.getElementsByClassName(".typed-row").innerHTML = typedFunction();
---
+++ assets/js/modules.js
@@ -0,0 +1,15 @@
+var Typed = require('typed.js')
+
+export function typedFunction() {
+    window.addEventListener('load', (event) => {
+        var first_row = new Typed('.typed-row', {
+            strings: [
+                'Budi izvrstan u onom što vidiš!',
+                'Budi izvrstan u onom što voliš.',
+                'Budi izvrstan u onom što voliš.<br><strong style="color: darkred">ZAISKRI</strong>.'
+            ],
+            startDelay: 100,
+            typeSpeed: 80
+        });
+    });
+}
