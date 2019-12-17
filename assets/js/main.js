326ac72162b4fbd1782c585fa2e771e2fa636184
Kevin Furjan <kfurjan@gmail.com>
2019-12-17 12:04:48 +0100

    Fixing stuff

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var Typed = require('typed.js')
+
+window.addEventListener('load', (event) => {
+    var first_row = new Typed('.typed-row', {
+        strings: [
+            'Budi izvrstan u onom što vidiš!',
+            'Budi izvrstan u onom što voliš.',
+            'Budi izvrstan u onom što voliš.<br><strong style="color: darkred">ZAISKRI</strong>.'
+        ],
+        startDelay: 100,
+        typeSpeed: 80,
+        contentType: 'html'
+    });
+});
